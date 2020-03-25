313af793a0a5814718a75db85817cf32c0d41b5b
natasyaadelia <53499671+natasyaadelia@users.noreply.github.com>
2020-03-25 14:35:02 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+
+<script src="http://localhost/php/siperpus/aset/jquery.js"></script>
+<script src="http://localhost/php/siperpus/aset/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+    $koneksi = mysqli_connect("localhost", "root", "", "db_perpus");
+
+    //cek koneksi
+
+    if(!$koneksi){
+        echo "";
+    }else{
+        echo "";
+    }
+?>
